a6fbf2698bf5d6d06a289e0019e2aa7a18b57a25
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-02-16 11:48:17 +0900

    add seeds and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.delete_all
+
+puts 'Creating 5 restaurants'
+
+5.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_address
+  category = Restaurant::CATEGORIES.sample
+  phone = Faker::PhoneNumber.phone_number
+  Restaurant.create!(name: name, address: address, category: category, phone_number: phone)
+end
+
+puts "Created #{Restaurant.count} restaurants"
